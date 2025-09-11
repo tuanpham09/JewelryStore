e6fbfe90eba1fec64d0f8ab0f6bcb5139125c778
tuanpham09 <146125035+tuanpham09@users.noreply.github.com>
2025-09-11 22:29:08 +0700

    Initialize backend and frontend skeleton

---
+++ backend/src/main/java/com/jewelrystore/JewelryStoreApplication.java
@@ -0,0 +1,11 @@
+package com.jewelrystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelryStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JewelryStoreApplication.class, args);
+    }
+}
